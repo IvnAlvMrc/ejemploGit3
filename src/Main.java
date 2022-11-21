59ad20381965d866833500a6f2c48c3b46c30bc0
a17ivanam <ivanalvesvigo@gmail.com>
2022-11-21 17:17:02 +0100

    Añadiendo código para probar las propiedades de Java

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,13 @@
 public class Main {
 
     public static void main(String[] args) {
-        public static void main(String[] args) {
-            System.out.println("Hello world!");
 
-            Empleado p = new Empleado("Pepe Pérez", 30);
-            System.out.println(p);
+        Empleado p = new Empleado("Pepe Pérez", 30);
+        System.out.println(p);
 
-            p.nombre = "Otro nombre";
-            System.out.println(p);
+        p.nombre = "Otro nombre";
+        System.out.println(p);
 
-        }
     }
 
-}
 }
