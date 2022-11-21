c9c99e46e71b5f38e3f005996648f8c01e0d1f79
a17ivanam <ivanalvesvigo@gmail.com>
2022-11-21 17:26:45 +0100

    Bug corregido y probado.

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,15 @@
 public class Main {
-<<<<<<< HEAD
 
     public static void main(String[] args) {
 
         Empleado p = new Empleado("Pepe Pérez", 30);
         System.out.println(p);
 
-        p.nombre = "Otro nombre";
+/*
+        p.nombre = "Otro nombre";  // Si intentamos cambiar el campo nombre, nos da un error de compilación
         System.out.println(p);
+*/
 
     }
 
-=======
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
->>>>>>> d3d35290f093ee5018bd13995c7456d4005e9d17
 }
